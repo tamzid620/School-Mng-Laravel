f86ac7d488f0cb81cbbe49ba402ae3e74d7da7d6
tamzid620 <tamzid620@gmail.com>
2025-12-14 11:24:45 +0600

    vite config file updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+        build: {
+        outDir: 'dist',   // 👈 Vercel will now find this
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
